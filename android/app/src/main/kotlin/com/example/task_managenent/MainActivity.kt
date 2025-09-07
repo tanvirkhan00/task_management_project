962d54c85e223567cb4fd376a8a03be35c4277eb
Tanvir Khan <kmtanvir1111@gmail.com>
2025-09-07 17:54:14 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/task_managenent/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_managenent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TaskManager extends StatelessWidget {
+  const TaskManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
