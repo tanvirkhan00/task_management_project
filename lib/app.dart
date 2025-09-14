894864ea3e01ed7d9d4cc0a14ee82e2d0c624c0b
Tanvir Khan <kmtanvir1111@gmail.com>
2025-09-14 18:47:55 +0600

    complete login screen

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,42 @@ class TaskManager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w600
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+            fillColor: Colors.white,
+            filled: true,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16),
+            hintStyle: TextStyle(
+                color: Colors.grey
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none
+            ),
+            errorBorder: OutlineInputBorder(
+                borderSide: BorderSide.none
+            )
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+              fixedSize: Size.fromWidth(double.maxFinite),
+              padding: EdgeInsets.symmetric(vertical: 12),
+              backgroundColor: Colors.green,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+              )
+          ),
+        )
+      ),
       home: SplashScreen(),
     );
   }
--- lib/ui/Screen/login_screen.dart
+++ lib/ui/Screen/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_managenent/ui/widgets/screen_background.dart';
 
@@ -9,6 +10,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,55 +21,70 @@ class _LoginScreenState extends State<LoginScreen> {
             child: ScreenBackground(
               child: Padding(
                 padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 80,),
-                    Text('Get Started With', style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600
-                    ),),
-                    const SizedBox(height: 24,),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        fillColor: Colors.white,
-                        filled: true,
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
-                        hintStyle: TextStyle(
-                          color: Colors.grey
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 80,),
+                      Text('Get Started With',
+                          style: Theme.of(context).textTheme.titleLarge),
+                      const SizedBox(height: 24,),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email'
                         ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none
-                        )
                       ),
-                    ),
-                    const SizedBox(height: 8,),
-                    TextFormField(),
-                    const SizedBox(height: 8,),
-                    FilledButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined)),
-                    TextButton(onPressed: (){} ,
-                        child: Text('Forget Your Password')
-                    ),
-                    RichText(text: TextSpan(
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18
+                      const SizedBox(height: 8,),
+                      TextFormField(
+                        controller: _passController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password'
+                        ),
                       ),
-                      text: "Don't have any account",
-                      children: [TextSpan(text: 'Sign Up')]
-                    ))
-                  ],
+                      const SizedBox(height: 8,),
+                      FilledButton(onPressed: (){},
+                          child: Icon(Icons.arrow_circle_right_outlined)),
+                      const SizedBox( height: 36,),
+                      Center(
+                        child: Column(
+                          children: [
+                            TextButton(onPressed: (){} ,
+                                child: Text('Forget Password', style: TextStyle(
+                                  color: Colors.grey
+                                ),)
+                            ),
+                            RichText(text: TextSpan(
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600
+                              ),
+                              text: "Don't have any account? ",
+                              children: [TextSpan(text: 'Sign Up', style: TextStyle(
+                                color: Colors.green
+                              ),
+                                recognizer: TapGestureRecognizer()..onTap = () {}
+                              )]
+                            )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             )
         ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passController.dispose();
+    super.dispose();
   }
 }
