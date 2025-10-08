07b9cbc032bdae2af117e61a9aaa1851565a0763
tanvirkhan00 <kmtanvir1111@gmail.com>
2025-10-08 20:58:01 +0600

    api call to add new task

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -3,4 +3,5 @@ class Urls {
 
   static const String registrationUrl = "$_baseUrl/Registration";
   static const String loginUrl = "$_baseUrl/Login";
+  static const String createTask = "$_baseUrl/createTask";
 }
