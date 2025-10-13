c7e75c08dd8fa90b10b6e256f5251e72f64bbe84
tanvirkhan00 <kmtanvir1111@gmail.com>
2025-10-13 20:45:05 +0600

    Api call for new task and progress task

--- lib/ui/Screen/progressTaskScreen.dart
+++ lib/ui/Screen/progressTaskScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:task_managenent/ui/widgets/task_count_card.dart';
-
+import '../../data/models/task_model.dart';
+import '../../data/services/apiCaller.dart';
+import '../../data/utils/urls.dart';
+import '../widgets/snack_bar_message.dart';
 import '../widgets/task-card.dart';
 
 class Progresstaskscreen extends StatefulWidget {
@@ -13,29 +15,56 @@ class Progresstaskscreen extends StatefulWidget {
 }
 
 class _ProgresstaskscreenState extends State<Progresstaskscreen> {
+
+  bool _getProgressTaskInProgress = false;
+  List<TaskModel> _progressTaskList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getAllProgressTask();
+  }
+
+  Future<void> _getAllProgressTask() async {
+    _getProgressTaskInProgress = true;
+    setState(() {});
+    final ApiResponse response = await apiCaller.getRequest(url: Urls.progressTaskListUrl);
+    if(response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.responseData['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _progressTaskList = list ;
+    } else {
+      showSnakbarMessage(context, response.errorMessage!);
+    }
+    _getProgressTaskInProgress = false;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            SizedBox(
-              height: 100,
-              child: ListView.separated(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return TCStatusCard(title: 'New', count: 05);
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 08);
-                },
-              ),
-            ),
-            // taskCard(),
-          ],
+        child: Visibility(
+          visible: _getProgressTaskInProgress == false,
+          replacement: Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.separated(
+            itemCount: _progressTaskList.length,
+            itemBuilder: (context, index) {
+              return taskCard(taskModel: _progressTaskList[index],
+                  refreshParent: (){
+                   _getAllProgressTask();
+                  });
+            },
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 8);
+            },
+          ),
         ),
       ),
     );
--- lib/ui/widgets/task-card.dart
+++ lib/ui/widgets/task-card.dart
@@ -1,43 +1,137 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_managenent/data/models/task_model.dart';
+import 'package:task_managenent/data/services/apiCaller.dart';
+import 'package:task_managenent/data/utils/urls.dart';
+import 'package:task_managenent/ui/widgets/snack_bar_message.dart';
 
-class taskCard extends StatelessWidget {
-  const taskCard({super.key, required this.taskModel,});
+class taskCard extends StatefulWidget {
+  const taskCard({super.key, required this.taskModel, required this.refreshParent});
 
-  final TaskModel taskModel ;
+  final TaskModel taskModel;
+  final VoidCallback refreshParent;
+
+  @override
+  State<taskCard> createState() => _taskCardState();
+}
+
+class _taskCardState extends State<taskCard> {
+  bool _changeStatusInProgress = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       tileColor: Colors.white,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8)
-      ),
-      title: Text(taskModel.title),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      title: Text(widget.taskModel.title),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         spacing: 8,
         children: [
-          Text(taskModel.description, style: TextStyle(color: Colors.grey),),
-          Text(taskModel.createdDate),
+          Text(
+            widget.taskModel.description,
+            style: TextStyle(color: Colors.grey),
+          ),
+          Text(widget.taskModel.createdDate),
           Row(
             children: [
               Chip(
-                label: Text(taskModel.status),
+                label: Text(widget.taskModel.status),
                 backgroundColor: Colors.blue,
-                labelStyle: TextStyle(
-                    color: Colors.white
-                ),
+                labelStyle: TextStyle(color: Colors.white),
               ),
               Spacer(),
-              IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
-              IconButton(onPressed: (){}, icon: Icon(Icons.edit, color: Colors.blue,))
+              IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.delete, color: Colors.red),
+              ),
+              Visibility(
+                visible: _changeStatusInProgress == false,
+                replacement: Center(
+                  child: CircularProgressIndicator(),
+                ),
+                child: IconButton(
+                  onPressed: _showChangeStatusDialog,
+                  icon: Icon(Icons.edit, color: Colors.blue),
+                ),
+              ),
             ],
-          )
+          ),
         ],
       ),
-
     );
+  }
+
+  void _showChangeStatusDialog() {
+    showDialog(
+      context: context,
+      builder: (ctx) {
+        return AlertDialog(
+          title: Text('Change Status'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                onTap: () {
+                  _changeStatus('New');
+                },
+                title: Text('New'),
+                trailing: widget.taskModel.status == "New"
+                    ? Icon(Icons.done)
+                    : null,
+              ),
+              ListTile(
+                onTap: () {
+                  _changeStatus('Progress');
+                },
+                title: Text('Progress'),
+                trailing: widget.taskModel.status == "Progress"
+                    ? Icon(Icons.done)
+                    : null,
+              ),
+              ListTile(
+                onTap: () {
+                  _changeStatus('Cancelled');
+                },
+                title: Text('Cancelled'),
+                trailing: widget.taskModel.status == "Cancelled"
+                    ? Icon(Icons.done)
+                    : null,
+              ),
+              ListTile(
+                onTap: () {
+                  _changeStatus('Completed');
+                },
+                title: Text('Completed'),
+                trailing: widget.taskModel.status == "Completed"
+                    ? Icon(Icons.done)
+                    : null,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _changeStatus(String status) async {
+    if(status == widget.taskModel.status) {
+      return ;
+    }
+
+    Navigator.pop(context);
+
+    _changeStatusInProgress = true;
+    setState(() {});
+
+    final ApiResponse response = await apiCaller.getRequest(
+      url: Urls.updateTaskStatus(widget.taskModel.id, status),);
+    _changeStatusInProgress = false;
+    setState(() {});
+    if(response.isSuccess) {
+      widget.refreshParent();
+    } else {
+      showSnakbarMessage(context, response.errorMessage!);
+    }
   }
 }
